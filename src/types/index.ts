d873a0ccc3e2de98caf3735e277a53e5c59f085d
BOTREL Kilian <botrel.kilian1999@gmail.com>
2023-08-16 20:51:43 +0200

    feat: basic interface and enums

---
+++ src/types/index.ts
@@ -0,0 +1,78 @@
+import type {
+  ClientEvents,
+  CommandInteraction,
+  SlashCommandBuilder,
+} from 'discord.js'
+
+export interface DiscordBotEventHandler<
+  T extends keyof ClientEvents = keyof ClientEvents,
+> {
+  /**
+   * Must be one of [Discord.js handled events](https://old.discordjs.dev/#/docs/discord.js/main/typedef/Events).
+   */
+  readonly name: T
+  /**
+   * Function that will be execute once the event is received with aditionnal data if needed.
+   */
+  readonly callback: (...parameters: ClientEvents[T]) => void | Promise<void>
+  /**
+   * If the event should be tracked once or indefinitely.
+   */
+  readonly once?: boolean
+}
+
+export interface InteractionMetadata {
+  /**
+   * Unique ID to track the request.
+   */
+  readonly requestId: string
+  /**
+   * User ID of the user who sent the interaction.
+   */
+  readonly userId: string
+  /**
+   * Channel ID where the interaction was sent.
+   */
+  channelId: string
+  /**
+   * The command name if the interaction is a command.
+   */
+  readonly commandName?: string
+  /**
+   * The command optional arguments if the interaction is a command.
+   */
+  readonly commandArguments?: Record<string, unknown>
+}
+
+export interface DiscordBotCommandHandler {
+  /**
+   * An instance of [SlashCommandBuilder](https://discordjs.guide/slash-commands/response-methods.html#command-response-methods) that defines how the command should be used.
+   */
+  readonly command: SlashCommandBuilder
+  /**
+   * Function that will be execute once the command is received with aditionnal data if needed.
+   */
+  readonly callback: (
+    interaction: CommandInteraction,
+    metadata?: InteractionMetadata,
+  ) => void | Promise<void>
+}
+
+export enum LogLevel {
+  /**
+   * Used when something doesn't behave as expected.
+   */
+  Error = 'error',
+  /**
+   *  Used when something is not critical but should be fixed.
+   */
+  Warn = 'warn',
+  /**
+   * Used when something should be kept track of.
+   */
+  Info = 'info',
+  /**
+   * Used mostly in development to track the flow of the application.
+   */
+  Debug = 'debug',
+}
