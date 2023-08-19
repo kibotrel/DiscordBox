08cc7a3e26c84901178ef8109488a3e1f1e65651
BOTREL Kilian <botrel.kilian1999@gmail.com>
2023-08-20 01:07:09 +0200

    fix: environment evaluation

--- src/misc/environments.ts
+++ src/misc/environments.ts
@@ -1,14 +1,23 @@
 /**
  * Check if the current environment is development.
+ * @returns whether the current environment is development.
  */
-export const isDevelopmentEnvironment = process.env.NODE_ENV === 'development'
+export const isDevelopmentEnvironment = () => {
+  return process.env.NODE_ENV === 'development'
+}
 
 /**
  * Check if the current environment is production.
+ * @returns whether the current environment is production.
  */
-export const isProductionEnvironment = process.env.NODE_ENV === 'production'
+export const isProductionEnvironment = () => {
+  return process.env.NODE_ENV === 'production'
+}
 
 /**
  * Check if the current environment is test.
+ * @returns whether the current environment is test.
  */
-export const isTestEnvironment = process.env.NODE_ENV === 'test'
+export const isTestEnvironment = () => {
+  return process.env.NODE_ENV === 'test'
+}
