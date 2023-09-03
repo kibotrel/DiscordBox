278a951978518a28b7eede35d1153e23740e6813
BOTREL Kilian <botrel.kilian1999@gmail.com>
2023-09-03 05:56:03 +0200

    chore: remove comments and order types

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,54 +7,30 @@ export interface EventHandler<
    * Must be one of [Discord.js handled events](https://old.discordjs.dev/#/docs/discord.js/main/typedef/Events).
    */
   readonly name: Type
-  /**
-   * Function that will be execute once the event is received with aditionnal data if needed.
-   */
   readonly callback: (
     ...parameters: DiscordJS.ClientEvents[Type]
   ) => void | Promise<void>
-  /**
-   * If the event should be tracked once or indefinitely.
-   */
   readonly once?: boolean
 }
 
 export interface InteractionMetadata {
-  /**
-   * Unique ID to track the request.
-   */
   readonly requestId: string
-  /**
-   * User ID of the user who sent the interaction.
-   */
   readonly userId: string
-  /**
-   * Channel ID where the interaction was sent.
-   */
   channelId: string
-  /**
-   * The command name if the interaction is a command.
-   */
   readonly commandName?: string
-  /**
-   * The command optional arguments if the interaction is a command.
-   */
   readonly commandArguments?: Record<string, unknown>
-  /**
-   * Action name if the interaction is a button.
-   */
   readonly actionName?: string
   readonly selectedOptions?: string[]
 }
 
 export type InteractionTypes =
-  | DiscordJS.CommandInteraction
   | DiscordJS.ButtonInteraction
+  | DiscordJS.ChannelSelectMenuInteraction
+  | DiscordJS.CommandInteraction
+  | DiscordJS.MentionableSelectMenuInteraction
+  | DiscordJS.RoleSelectMenuInteraction
   | DiscordJS.StringSelectMenuInteraction
   | DiscordJS.UserSelectMenuInteraction
-  | DiscordJS.RoleSelectMenuInteraction
-  | DiscordJS.MentionableSelectMenuInteraction
-  | DiscordJS.ChannelSelectMenuInteraction
 
 export interface InteractionHandler<
   InteractionType extends InteractionTypes = InteractionTypes,
@@ -63,9 +39,6 @@ export interface InteractionHandler<
    * An instance of [SlashCommandBuilder](https://discordjs.guide/slash-commands/response-methods.html#command-response-methods) if the interaction is a command.
    */
   readonly action: DiscordJS.SlashCommandBuilder | string
-  /**
-   * Function that will be execute once the command is received with aditionnal data if needed.
-   */
   readonly callback: (
     interaction: InteractionType,
     metadata?: InteractionMetadata,
