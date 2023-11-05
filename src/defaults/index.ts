54b7ea038d96d36c41d985c9746bab7fc9488e5d
BOTREL Kilian <botrel.kilian1999@gmail.com>
2023-11-05 03:57:42 +0100

    feat: get error call stack

--- src/defaults/index.ts
+++ src/defaults/index.ts
@@ -1,2 +1,4 @@
+export * from './components/index.js'
 export * from './constants.js'
 export * from './events/index.js'
+export * from './interactions/index.js'
---
+++ src/defaults/interactions/getErrorLog.ts
@@ -0,0 +1,48 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import url from 'node:url'
+
+import type { ButtonBuilder } from 'discord.js'
+import DiscordJS from 'discord.js'
+
+import * as Classes from '../../classes/index.js'
+import * as Misc from '../../miscs/index.js'
+import type * as Types from '../../types/index.js'
+import * as Defaults from '../index.js'
+
+const dirname = url.fileURLToPath(new URL('.', import.meta.url))
+const logDirectory = path.join(dirname, '../../../logs')
+
+export const getErrorLog = (): Types.InteractionHandler => {
+  return {
+    action: 'getErrorLog',
+    callback: async (interaction, metadata) => {
+      const updatableInteraction =
+        interaction as DiscordJS.MessageComponentInteraction
+      const fileName = metadata?.additionalData
+
+      await updatableInteraction.update({
+        components: [
+          new DiscordJS.ActionRowBuilder<ButtonBuilder>().addComponents(
+            Defaults.sendReportButton({ requestId: '', isDisabled: true }),
+          ),
+        ],
+      })
+
+      if (!fileName) {
+        throw new Classes.InternalError(
+          'No file name provided in additional data.',
+        )
+      }
+
+      const filePath = path.join(logDirectory, `${fileName}.log`)
+      const fileContent = await fs.readFile(filePath, 'utf8')
+
+      await updatableInteraction.followUp({
+        content: Misc.codeBlock(fileContent),
+        ephemeral: true,
+      })
+      await fs.unlink(filePath)
+    },
+  }
+}
