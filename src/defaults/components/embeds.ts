f9773eee15468f1a0849332f36a4b08b5a417cd6
BOTREL Kilian <botrel.kilian1999@gmail.com>
2023-11-05 03:16:49 +0100

    feat: error embed

---
+++ src/defaults/components/embeds.ts
@@ -0,0 +1,27 @@
+import DiscordJS from 'discord.js'
+
+import * as Constants from '../../constants/index.js'
+import * as Defaults from '../../defaults/index.js'
+import * as Misc from '../../miscs/index.js'
+
+export const errorEmbed = (errorMessage: string): DiscordJS.EmbedBuilder => {
+  const embed = new DiscordJS.EmbedBuilder()
+    .setTitle('Something went wrong!')
+    .setColor(Defaults.EmbedColors.Error)
+  const reportMessage =
+    'If the error persists, use the button below to report it. Thank you!'
+
+  if (errorMessage === Constants.ErrorMessages.InternalError) {
+    embed.setDescription(
+      `An unexpected or internal error occurred while processing your request. Please try again in few seconds.\n\u200B\n${reportMessage}`,
+    )
+  } else {
+    embed.setDescription(
+      `An error occurred while processing your request. Check the details below and try again if needed.\n\u200B\n:mag: **Description**\n${Misc.codeBlock(
+        errorMessage,
+      )}\n${reportMessage}`,
+    )
+  }
+
+  return embed
+}
